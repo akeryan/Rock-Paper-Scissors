01cb8f0ee92b6513ace1f75a34f3665b31d281e3
Aram Keryan <aram.keryan@gmail.com>
2023-07-17 15:50:00 +0400

    Update scripts.js
    incorporated playRound() function

--- scripts.js
+++ scripts.js
@@ -16,26 +16,29 @@ function getMyChoice() {
 const computerSelection = getComputerChoice();
 const playerSelection = getMyChoice();
 
-console.log(`Oponent: ${compueterSelection}`);
+console.log(`Oponent: ${computerSelection}`);
 console.log(`You: ${playerSelection}`);
 
-let result = "";
+function playRound(playerSelection, computerSelection) {
+    let result = "";
+    
+    if (playerSelection != computerSelection) {
+        switch(playerSelection) {
+            case "rock": result = computerSelection === "paper" ? "You Lose! Paper beats Rock" : "You Win! Rock beats Scissors";
+            break;
+            case "paper": result = computerSelection === "scissors" ? "You Lose! Scissors beat Paper" : "You Win! Paper beats Rock";
+            break;
+            case "scissors": result = computerSelection === "rock" ? "You Lose! Rock beats Scissors" : "You Win! Scissors beat Paper";
+            break;
+        }
+    } else {
+        result = "draw"
+    }   
 
-if (playerSelection != compueterSelection) {
-    switch(me) {
-        case "rock": result = computerSelection === "paper" ? "You Lose! Paper beats Rock" : "You Win! Rock beats Scissors";
-        break;
-        case "paper": result = computerSelection === "scissors" ? "You Lose! Scissors beat Paper" : "You Win! Paper beats Rock";
-        break;
-        case "scissors": result = computerSelection === "rock" ? "You Lose! Rock beats Scissors" : "You Win! Scissors beat Paper";
-        break;
-    }
-
-} else {
-    result = "draw";
+    return result;
 }
 
-console.log("Result: " + result);
+console.log("Result: " + playRound(playerSelection, computerSelection));
 
 
 
