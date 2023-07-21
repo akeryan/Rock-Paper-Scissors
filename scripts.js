4bbd3a3344798cb1b0625540398ac5434be98fda
Aram Keryan <13392202+akeryan@users.noreply.github.com>
2023-07-21 20:05:04 +0400

    added buttons + logic for one round play

--- scripts.js
+++ scripts.js
@@ -1,6 +1,22 @@
 const choices = ["rock", "paper", "scissors"]
 const score = [0, 0] // score[0] - player, score[1] - computer
 
+const rockBtn = document.getElementById('rock-btn')
+const paperBtn = document.getElementById('paper-btn')
+const scissorsBtn = document.getElementById('scissors-btn')
+
+rockBtn.addEventListener('click', () => {
+    playRound('rock', getComputerChoice())
+})
+
+paperBtn.addEventListener('click', () => {
+    playRound('paper', getComputerChoice())
+})
+
+scissorsBtn.addEventListener('click', () => {
+    playRound('scissors', getComputerChoice())
+})
+
 function getComputerChoice() {
     const rand = Math.floor(Math.random() * 3);
     return choices[rand];
@@ -30,50 +46,50 @@ function playRound(playerSelection, computerSelection) {
         result = "draw"
     }   
 
-    return result;
+    console.log(result)
 }
 
-function game() {
-    const numOfRounds = 5;
-    let currentRound = 1;
-    let result = ""
+// function game() {
+//     const numOfRounds = 5;
+//     let currentRound = 1;
+//     let result = ""
 
-    let cSelection = ""
-    let pSelection = ""
+//     let cSelection = ""
+//     let pSelection = ""
 
-    while(currentRound <= numOfRounds) {
-        console.log(`Round ${currentRound}:`);
+//     while(currentRound <= numOfRounds) {
+//         console.log(`Round ${currentRound}:`);
 
-        cSelection = getComputerChoice();
-        pSelection = getMyChoice();
+//         cSelection = getComputerChoice();
+//         pSelection = getMyChoice();
 
-        console.log(`  Computer: ${cSelection}`);
-        console.log(`  Player: ${pSelection}`);
+//         console.log(`  Computer: ${cSelection}`);
+//         console.log(`  Player: ${pSelection}`);
 
-        result = playRound(pSelection, cSelection)
+//         result = playRound(pSelection, cSelection)
 
-        if(result.includes('draw')) {
-            score[0]++
-            score[1]++
-        } else if (result.includes("You Lose")) {
-            score[1]++
-        } else score[0]++
+//         if(result.includes('draw')) {
+//             score[0]++
+//             score[1]++
+//         } else if (result.includes("You Lose")) {
+//             score[1]++
+//         } else score[0]++
 
-        console.log("  " + result);
-        currentRound++;
-    }  
+//         console.log("  " + result);
+//         currentRound++;
+//     }  
 
-    console.log("------------------")
-    console.log("Total Score: ")
-    console.log(`  Player: ${score[0]}, Computer: ${score[1]}`)
-    if(score[0] > score[1]) {
-        console.log("Player is the Winner")
-    } else if (score[0] < score[1]) {
-        console.log("Computer is the Winner")
-    } else console.log("DRAW")
-}
+//     console.log("------------------")
+//     console.log("Total Score: ")
+//     console.log(`  Player: ${score[0]}, Computer: ${score[1]}`)
+//     if(score[0] > score[1]) {
+//         console.log("Player is the Winner")
+//     } else if (score[0] < score[1]) {
+//         console.log("Computer is the Winner")
+//     } else console.log("DRAW")
+// }
 
-game();
+// game();
 
 
 
